2cd04aaa84530ff0859787ef0ba013a1b3b86d1e
Steve Dower <steve.dower@python.org>
2025-11-13 21:06:11 +0000

    Adds drop handler and MSI registration. (#218)
    Fixes #217

--- src/pyshellext/shellext.cpp
+++ src/pyshellext/shellext.cpp
@@ -1,8 +1,14 @@
+#include <string>
+#include <vector>
+
 #define _WIN32_WINNT _WIN32_WINNT_WIN10
 #include <sdkddkver.h>
 
 #define __WRL_CLASSIC_COM__
 #include <wrl.h>
+
+#include <pathcch.h>
+#include <strsafe.h>
 
 using namespace Microsoft::WRL;
 
@@ -10,9 +16,20 @@ using namespace Microsoft::WRL;
 
 static HINSTANCE hModule;
 
+
 #define CLSID_IDLE_COMMAND "{C7E29CB0-9691-4DE8-B72B-6719DDC0B4A1}"
 #define CLSID_LAUNCH_COMMAND "{F7209EE3-FC96-40F4-8C3F-4B7D3994370D}"
 #define CLSID_COMMAND_ENUMERATOR "{F82C8CD5-A69C-45CC-ADC6-87FC5F4A7429}"
+#define CLSID_DRAGDROPSUPPORT "{EAF5E48F-F54A-4A03-824B-CA880772EE20}"
+
+#ifndef _DEBUG
+#undef OutputDebugString
+#define OutputDebugString(x)
+#endif
+
+
+static const WPARAM DDWM_UPDATEWINDOW = WM_USER + 3;
+static const LPCWSTR DRAG_MESSAGE = L"Open with %1";
 
 
 LRESULT RegReadStr(HKEY key, LPCWSTR valueName, std::wstring& result)
@@ -251,9 +268,9 @@ public:
             parameters.c_str(),
             NULL
         };
-        OutputDebugStringW(L"IdleCommand::Invoke");
-        OutputDebugStringW(exe.c_str());
-        OutputDebugStringW(parameters.c_str());
+        OutputDebugString(L"IdleCommand::Invoke");
+        OutputDebugString(exe.c_str());
+        OutputDebugString(parameters.c_str());
         ShellExecuteExW(&sei);
         return S_OK;
     }
@@ -402,7 +419,7 @@ public:
         if (FAILED(hr)) {
             wchar_t buffer[512];
             swprintf_s(buffer, L"IdleCommand error 0x%08X", (DWORD)hr);
-            OutputDebugStringW(buffer);
+            OutputDebugString(buffer);
             idles.clear();
         }
     }
@@ -514,7 +531,388 @@ public:
 };
 
 
+class DECLSPEC_UUID(CLSID_DRAGDROPSUPPORT) DragDropSupport : public RuntimeClass<
+    RuntimeClassFlags<ClassicCom>, IDropTarget, IPersistFile>
+{
+    std::wstring target, target_name, target_dir;
+    DWORD target_mode;
+
+    static CLIPFORMAT cfDropDescription;
+    static CLIPFORMAT cfDragWindow;
+
+    IDataObject *data_obj;
+
+public:
+    DragDropSupport() : data_obj(NULL) {
+        if (!cfDropDescription) {
+            cfDropDescription = RegisterClipboardFormat(CFSTR_DROPDESCRIPTION);
+        }
+        if (!cfDropDescription) {
+            OutputDebugString(L"PyShellExt::DragDropSupport - failed to get CFSTR_DROPDESCRIPTION format");
+        }
+        if (!cfDragWindow) {
+            cfDragWindow = RegisterClipboardFormat(L"DragWindow");
+        }
+        if (!cfDragWindow) {
+            OutputDebugString(L"PyShellExt::DragDropSupport - failed to get DragWindow format");
+        }
+    }
+
+    ~DragDropSupport() {
+        if (data_obj) {
+            data_obj->Release();
+        }
+    }
+
+    HRESULT UpdateDropDescription(DROPDESCRIPTION *drop_desc) {
+        StringCchCopy(drop_desc->szMessage, sizeof(drop_desc->szMessage) / sizeof(drop_desc->szMessage[0]), DRAG_MESSAGE);
+        StringCchCopy(drop_desc->szInsert, sizeof(drop_desc->szInsert) / sizeof(drop_desc->szInsert[0]), target_name.c_str());
+        drop_desc->type = DROPIMAGE_MOVE;
+        return S_OK;
+    }
+
+    HRESULT UpdateDropDescription(IDataObject *pDataObj) {
+        STGMEDIUM medium;
+        FORMATETC fmt = {
+            cfDropDescription,
+            NULL,
+            DVASPECT_CONTENT,
+            -1,
+            TYMED_HGLOBAL
+        };
+
+        auto hr = pDataObj->GetData(&fmt, &medium);
+        if (FAILED(hr)) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::UpdateDropDescription - failed to get DROPDESCRIPTION format");
+            return hr;
+        }
+        if (!medium.hGlobal) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::UpdateDropDescription - DROPDESCRIPTION format had NULL hGlobal");
+            ReleaseStgMedium(&medium);
+            return E_FAIL;
+        }
+        auto drop_desc = (DROPDESCRIPTION*)GlobalLock(medium.hGlobal);
+        if (!drop_desc) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::UpdateDropDescription - failed to lock DROPDESCRIPTION hGlobal");
+            ReleaseStgMedium(&medium);
+            return E_FAIL;
+        }
+        hr = UpdateDropDescription(drop_desc);
+
+        GlobalUnlock(medium.hGlobal);
+        ReleaseStgMedium(&medium);
+
+        return hr;
+    }
+
+    HRESULT GetDragWindow(IDataObject *pDataObj, HWND *phWnd) {
+        HRESULT hr;
+        HWND *pMem;
+        STGMEDIUM medium;
+        FORMATETC fmt = {
+            cfDragWindow,
+            NULL,
+            DVASPECT_CONTENT,
+            -1,
+            TYMED_HGLOBAL
+        };
+
+        hr = pDataObj->GetData(&fmt, &medium);
+        if (FAILED(hr)) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetDragWindow - failed to get DragWindow format");
+            return hr;
+        }
+        if (!medium.hGlobal) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetDragWindow - DragWindow format had NULL hGlobal");
+            ReleaseStgMedium(&medium);
+            return E_FAIL;
+        }
+
+        pMem = (HWND*)GlobalLock(medium.hGlobal);
+        if (!pMem) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetDragWindow - failed to lock DragWindow hGlobal");
+            ReleaseStgMedium(&medium);
+            return E_FAIL;
+        }
+
+        *phWnd = *pMem;
+
+        GlobalUnlock(medium.hGlobal);
+        ReleaseStgMedium(&medium);
+
+        return S_OK;
+    }
+
+    HRESULT GetArgumentsW(LPCWSTR files, LPCWSTR *pArguments) {
+        std::wstring arg_str;
+        while (arg_str.size() < 32767 && *files) {
+            std::wstring wfile(files);
+            files += wfile.size() + 1;
+
+            if (wfile.find(L' ') != wfile.npos) {
+                wfile.insert(wfile.begin(), L'"');
+                wfile.push_back(L'"');
+            }
+            if (arg_str.size()) {
+                arg_str.push_back(L' ');
+            }
+            arg_str += wfile;
+        }
+
+        LPWSTR args = (LPWSTR)CoTaskMemAlloc(sizeof(WCHAR) * (arg_str.size() + 1));
+        *pArguments = args;
+        if (!args) {
+            return E_OUTOFMEMORY;
+        }
+        wcscpy_s(args, arg_str.size() + 1, arg_str.c_str());
+
+        return S_OK;
+    }
+
+    HRESULT GetArgumentsA(LPCSTR files, LPCWSTR *pArguments) {
+        std::string arg_str;
+        while (arg_str.size() < 32767 && *files) {
+            std::string file(files);
+            files += file.size() + 1;
+
+            if (file.find(' ') != file.npos) {
+                file.insert(file.begin(), '"');
+                file.push_back('"');
+            }
+            if (arg_str.size()) {
+                arg_str.push_back(' ');
+            }
+            arg_str += file;
+        }
+
+        int wlen = MultiByteToWideChar(CP_ACP, 0, arg_str.data(), arg_str.size(), NULL, 0);
+        if (!wlen) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetArguments - failed to get length of wide-char path");
+            return E_FAIL;
+        }
+
+        LPWSTR args = (LPWSTR)CoTaskMemAlloc(sizeof(WCHAR) * (wlen + 1));
+        if (!args) {
+            return E_OUTOFMEMORY;
+        }
+        wlen = MultiByteToWideChar(CP_ACP, 0, arg_str.data(), arg_str.size(), args, wlen + 1);
+        if (!wlen) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetArguments - failed to convert multi-byte to wide-char path");
+            CoTaskMemFree(args);
+            return E_FAIL;
+        }
+        args[wlen] = '\0';
+        *pArguments = args;
+        return S_OK;
+    }
+
+    HRESULT GetArguments(IDataObject *pDataObj, LPCWSTR *pArguments) {
+        HRESULT hr;
+        DROPFILES *pdropfiles;
+
+        STGMEDIUM medium;
+        FORMATETC fmt = {
+            CF_HDROP,
+            NULL,
+            DVASPECT_CONTENT,
+            -1,
+            TYMED_HGLOBAL
+        };
+
+        hr = pDataObj->GetData(&fmt, &medium);
+        if (FAILED(hr)) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetArguments - failed to get CF_HDROP format");
+            return hr;
+        }
+        if (!medium.hGlobal) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetArguments - CF_HDROP format had NULL hGlobal");
+            ReleaseStgMedium(&medium);
+            return E_FAIL;
+        }
+
+        pdropfiles = (DROPFILES*)GlobalLock(medium.hGlobal);
+        if (!pdropfiles) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::GetArguments - failed to lock CF_HDROP hGlobal");
+            ReleaseStgMedium(&medium);
+            return E_FAIL;
+        }
+
+        if (pdropfiles->fWide) {
+            LPCWSTR files = (LPCWSTR)((char*)pdropfiles + pdropfiles->pFiles);
+            hr = GetArgumentsW(files, pArguments);
+        } else {
+            LPCSTR files = (LPCSTR)((char*)pdropfiles + pdropfiles->pFiles);
+            hr = GetArgumentsA(files, pArguments);
+        }
+
+        GlobalUnlock(medium.hGlobal);
+        ReleaseStgMedium(&medium);
+
+        return hr;
+    }
+
+    HRESULT NotifyDragWindow(HWND hwnd) {
+        LRESULT res;
+
+        if (!hwnd) {
+            return S_FALSE;
+        }
+
+        res = SendMessage(hwnd, DDWM_UPDATEWINDOW, 0, NULL);
+
+        if (res) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::NotifyDragWindow - failed to post DDWM_UPDATEWINDOW");
+            return E_FAIL;
+        }
+
+        return S_OK;
+    }
+
+    // IDropTarget implementation
+
+    STDMETHODIMP DragEnter(IDataObject *pDataObj, DWORD grfKeyState, POINTL pt, DWORD *pdwEffect) {
+        HWND hwnd = NULL;
+
+        OutputDebugString(L"PyShellExt::DragDropSupport::DragEnter");
+
+        if (data_obj) {
+            data_obj->Release();
+        }
+        pDataObj->AddRef();
+        data_obj = pDataObj;
+
+        *pdwEffect = DROPEFFECT_MOVE;
+
+        if (FAILED(UpdateDropDescription(data_obj))) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::DragEnter - failed to update drop description");
+        }
+        if (FAILED(GetDragWindow(data_obj, &hwnd))) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::DragEnter - failed to get drag window");
+        }
+        if (FAILED(NotifyDragWindow(hwnd))) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::DragEnter - failed to notify drag window");
+        }
+
+        return S_OK;
+    }
+
+    STDMETHODIMP DragLeave() {
+        if (data_obj) {
+            data_obj->Release();
+            data_obj = NULL;
+        }
+        return S_OK;
+    }
+
+    STDMETHODIMP DragOver(DWORD grfKeyState, POINTL pt, DWORD *pdwEffect) {
+        *pdwEffect = DROPEFFECT_MOVE;
+        return S_OK;
+    }
+
+    STDMETHODIMP Drop(IDataObject *pDataObj, DWORD grfKeyState, POINTL pt, DWORD *pdwEffect) {
+        LPCWSTR args;
+
+        OutputDebugString(L"PyShellExt::DragDropSupport::Drop");
+        *pdwEffect = DROPEFFECT_NONE;
+
+        if (pDataObj != data_obj) {
+            OutputDebugString(L"PyShellExt::DragDropSupport::Drop - unexpected data object");
+            return E_FAIL;
+        }
+
+        data_obj->Release();
+        data_obj = NULL;
+
+        if (SUCCEEDED(GetArguments(pDataObj, &args))) {
+            OutputDebugString(args);
+            ShellExecute(NULL, NULL, target.c_str(), args, target_dir.c_str(), SW_NORMAL);
+
+            CoTaskMemFree((LPVOID)args);
+        } else {
+            OutputDebugString(L"PyShellExt::DragDropSupport::Drop - failed to get launch arguments");
+        }
+
+        return S_OK;
+    }
+
+    // IPersistFile implementation
+
+    STDMETHODIMP GetCurFile(LPOLESTR *ppszFileName) {
+        HRESULT hr;
+        size_t len = target.size();
+
+        if (!ppszFileName) {
+            return E_POINTER;
+        }
+
+        *ppszFileName = (LPOLESTR)CoTaskMemAlloc(sizeof(WCHAR) * (len + 1));
+        if (!*ppszFileName) {
+            return E_OUTOFMEMORY;
+        }
+
+        hr = StringCchCopy(*ppszFileName, len + 1, target.c_str());
+        if (FAILED(hr)) {
+            CoTaskMemFree(*ppszFileName);
+            *ppszFileName = NULL;
+            return E_FAIL;
+        }
+
+        return S_OK;
+    }
+
+    STDMETHODIMP IsDirty() {
+        return S_FALSE;
+    }
+
+    STDMETHODIMP Load(LPCOLESTR pszFileName, DWORD dwMode) {
+        OutputDebugString(L"PyShellExt::DragDropSupport::Load");
+        OutputDebugString(pszFileName);
+
+        target = pszFileName;
+        target_dir = pszFileName;
+        switch (PathCchRemoveFileSpec(target_dir.data(), target_dir.size())) {
+        case S_OK:
+            target_dir.resize(wcsnlen_s(target_dir.data(), target_dir.size()));
+            target_name = { target.begin() + target_dir.size(), target.end() };
+            while (!target_name.empty() && (target_name.front() == L'\\' || target_name.front() == L'/')) {
+                target_name.erase(0, 1);
+            }
+            break;
+        case S_FALSE:
+            target_name = L"script";
+            break;
+        default:
+            OutputDebugString(L"PyShellExt::DragDropSupport::Load - failed to remove filespec from target");
+            return E_FAIL;
+        }
+
+        OutputDebugString(target.c_str());
+        target_mode = dwMode;
+        OutputDebugString(L"PyShellExt::DragDropSupport::Load - S_OK");
+        return S_OK;
+    }
+
+    STDMETHODIMP Save(LPCOLESTR pszFileName, BOOL fRemember) {
+        return E_NOTIMPL;
+    }
+
+    STDMETHODIMP SaveCompleted(LPCOLESTR pszFileName) {
+        return E_NOTIMPL;
+    }
+
+    STDMETHODIMP GetClassID(CLSID *pClassID) {
+        *pClassID = __uuidof(DragDropSupport);
+        return S_OK;
+    }
+};
+
+CLIPFORMAT DragDropSupport::cfDropDescription = 0;
+CLIPFORMAT DragDropSupport::cfDragWindow = 0;
+
+
 CoCreatableClass(IdleCommand);
+CoCreatableClass(DragDropSupport);
+
 
 #ifdef PYSHELLEXT_TEST
 
@@ -528,6 +926,47 @@ IExplorerCommand *MakeLaunchCommand(std::wstring title, std::wstring exe, std::w
 IExplorerCommand *MakeIdleCommand(HKEY hive, LPCWSTR root)
 {
     return Make<IdleCommand>(hive, root).Detach();
+}
+
+HRESULT GetDropArgumentsW(LPCWSTR args, std::wstring &parsed)
+{
+    LPCWSTR p;
+    auto o = Make<DragDropSupport>();
+    HRESULT hr = o->GetArgumentsW(args, &p);
+    if (SUCCEEDED(hr)) {
+        parsed = p;
+        CoTaskMemFree((LPVOID)p);
+    }
+    return hr;
+}
+
+HRESULT GetDropArgumentsA(LPCSTR args, std::wstring &parsed)
+{
+    LPCWSTR p;
+    auto o = Make<DragDropSupport>();
+    HRESULT hr = o->GetArgumentsA(args, &p);
+    if (SUCCEEDED(hr)) {
+        parsed = p;
+        CoTaskMemFree((LPVOID)p);
+    }
+    return hr;
+}
+
+HRESULT GetDropDescription(LPCOLESTR pszFileName, DWORD dwMode, std::wstring &message, std::wstring &insert)
+{
+    auto o = Make<DragDropSupport>();
+    HRESULT hr = o->Load(pszFileName, dwMode);
+    if (FAILED(hr)) {
+        return hr;
+    }
+    DROPDESCRIPTION drop_desc;
+    ZeroMemory(&drop_desc, sizeof(drop_desc));
+    hr = o->UpdateDropDescription(&drop_desc);
+    if (SUCCEEDED(hr)) {
+        message = drop_desc.szMessage;
+        insert = drop_desc.szInsert;
+    }
+    return hr;
 }
 
 #elif defined(_WINDLL)
